aed9b612a018f25374745d8ba1af7fd53f340b6b
Leandro Linardos <leandro.linardos@gmail.com>
2019-07-22 12:19:17 -0300

    Adds image to book cell.

--- LibraryList/ListOfBooks/BookCell.swift
+++ LibraryList/ListOfBooks/BookCell.swift
@@ -1,10 +1,11 @@
 import UIKit
 import Layout
+import SDWebImage
 
 class BookCell: UITableViewCell {
   private static var reuseId: String { return String(describing: BookCell.self) }
   
-  private var photoView = UIImageView()
+  private var photoView = PhotoView()
   private var line1 = UILabel()
   private var line2 = UILabel()
   private var line3 = UILabel()
@@ -22,9 +23,14 @@ class BookCell: UITableViewCell {
     })
     let lines = UIStackView(arrangedSubviews: labels)
     lines.axis = .vertical
+  
+    let body = UIStackView(arrangedSubviews: [photoView, lines])
+    body.axis = .horizontal
+    body.spacing = 8
     
-    contentView.addSubview(lines)
-    Layout().allign(.all, of: lines, and: contentView, insets: UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16))
+    contentView.addSubview(body)
+    
+    Layout().allign(.all, of: body, and: contentView, insets: UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16))
   }
   
   required init?(coder aDecoder: NSCoder) {
@@ -37,6 +43,7 @@ class BookCell: UITableViewCell {
     self.line3.text = "\(book.popularity)/100"
     self.line4.text = book.isAvailable ? "Disponible" : "No disponible"
     self.line4.textColor = book.isAvailable ? UIColor.blue : UIColor.red
+    self.photoView.setImage(book.imageURL)
   }
 }
 
@@ -48,4 +55,42 @@ extension BookCell {
     return tableView.dequeueReusableCell(withIdentifier: BookCell.reuseId) as! BookCell
   }
 
+}
+
+
+class PhotoView: UIView {
+  private var photoView = UIImageView()
+  private var spinner = UIActivityIndicatorView(style: .gray)
+  init() {
+    super.init(frame: .zero)
+    
+    photoView.contentMode = .scaleAspectFit
+    photoView.clipsToBounds = true
+    
+    let container = UIStackView(arrangedSubviews: [photoView])
+    container.alignment = .center
+    container.axis = .horizontal
+    
+    self.addSubview(container)
+    Layout().allign(.all, of: container, and: self)
+    
+    self.addSubview(spinner)
+    Layout().center(spinner, in: self)
+    
+    Layout().size(.width, of: photoView, is: 66)
+    Layout().size(.height, of: photoView, is: 66)
+  }
+  
+  required init?(coder aDecoder: NSCoder) { return nil }
+  
+  func setImage(_ url: URL) {
+    spinner.startAnimating()
+    photoView.backgroundColor = .clear
+    photoView.sd_setImage(with: url) { (_, error, _, _) in
+      self.spinner.stopAnimating()
+      if error != nil {
+        self.photoView.backgroundColor = .lightGray
+      }
+    }
+  }
 }
